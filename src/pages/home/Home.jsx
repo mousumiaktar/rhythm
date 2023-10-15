0189e93f7957120e0ca5e639d82e0632cc04c40e
Mousumi Mou <ridimarahmanmou5804@gmail.com>
2023-10-15 21:53:54 +0600

    updated

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,6 +3,7 @@ import DistributeOn from "./DistributeOn";
 import Featured from "./Featured";
 import Hero from "./Hero";
 import MakeMoney from "./MakeMoney";
+import MobileOperator from "./MobileOperator";
 import Plans from "./Plans";
 import Stores from "./Stores";
 import WhatWeDo from "./WhatWeDo";
@@ -20,6 +21,7 @@ const Home = () => {
             <Plans />
             <WithoutCt />
             <MakeMoney />
+            <MobileOperator />
         </>
     );
 };
