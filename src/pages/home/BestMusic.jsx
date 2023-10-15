aa2311d2f9b12e85306fb36eb8e3a16fad9a267a
Mousumi Mou <ridimarahmanmou5804@gmail.com>
2023-10-15 21:55:16 +0600

    best music

---
+++ src/pages/home/BestMusic.jsx
@@ -0,0 +1,25 @@
+const BestMusic = () => {
+    return (
+        <div className='mt-[250px] md:mt-40'>
+            <h3 className='font-semibold text-center'>Best Music Distribution India</h3>
+            <div className='bg-gradient-to-l from-[#693CFF] to-[#386CFE] py-8 mt-5'>
+                <div className='container mx-auto'>
+                    <div className='md:flex items-center justify-between text-white'>
+                        <div className='md:grid grid-cols-1 md:grid-cols-2 gap-5'>
+                            <div className='flex gap-3'>
+                                <h5>Why Think</h5>
+                                <p>|</p>
+                            </div>
+                            <p>Distribute Your Music Today! - Pay After Work Done</p>
+                        </div>
+                        <div>
+                            <button className='border-2 border-white py-2 px-4 rounded-full'>Apply Now</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default BestMusic;
