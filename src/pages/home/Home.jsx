6edd8ca3d777f3d1d2caea16be186612b33f77ac
Mousumi Mou <ridimarahmanmou5804@gmail.com>
2023-10-15 21:52:07 +0600

    code updated

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,6 +2,7 @@ import Distribute from "./Distribute";
 import DistributeOn from "./DistributeOn";
 import Featured from "./Featured";
 import Hero from "./Hero";
+import MakeMoney from "./MakeMoney";
 import Plans from "./Plans";
 import Stores from "./Stores";
 import WhatWeDo from "./WhatWeDo";
@@ -18,6 +19,7 @@ const Home = () => {
             <Featured />
             <Plans />
             <WithoutCt />
+            <MakeMoney />
         </>
     );
 };
